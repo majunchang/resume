0a53b0e8b1059846991ff8cc60b210afcc40f455
junchang.ma.ele_waimai <junchang.ma@ele.me>
2018-09-27 19:58:29 +0800

    完善部分信息

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
 import { Timeline } from 'antd'
 
-const TimelineItem = ( props ) => {
+const TimelineItem = (props) => {
   let { name, description, html_url } = props
 
   return (
     <Timeline.Item>
-        <div className="project-item">
-            <div className="item-title">
-                { name }
-                <a className="item-link"
-                  href={html_url}
-                  target="_blank">
-                    <i className="iconfont icon-link"></i>源代码
-                </a>
-            </div>
-            <div className="item-info">
-                <div className="item-desc">
-                    { description }
-                </div>
-            </div>
+      <div className='project-item'>
+        <div className='item-title'>
+          { name }
+          <a className='item-link'
+                href={html_url}
+                target='_blank'>
+                <i className='iconfont icon-link' />源代码
+              </a>
         </div>
+        <div className='item-info'>
+          <div className='item-desc'>
+            { description }
+          </div>
+        </div>
+      </div>
     </Timeline.Item>
   )
 }
--- src/layouts/NavMenu.js
+++ src/layouts/NavMenu.js
@@ -3,49 +3,48 @@ import { Menu, Icon } from 'antd'
 import { Link } from 'react-router'
 
 class NavMenu extends Component {
-
-  render(){
-    let { current, handleClick, collapse, onCollapseChange  } = this.props
+  render () {
+    let { current, handleClick, collapse, onCollapseChange } = this.props
 
     const MenuData = [
-      {name: "home", desc: "首页"},
-      {name: "about", desc: "关于我"},
-      {name: "skill", desc: "专业技能"},
-      {name: "project", desc: "项目经验"},
-      {name: "contact", desc: "联系我"}
+      {name: 'home', desc: '首页'},
+      {name: 'about', desc: '关于我'},
+      {name: 'skill', desc: '专业技能'},
+      {name: 'project', desc: '项目经验'},
+      {name: 'contact', desc: '联系我'}
     ]
 
     return (
-      <aside className="layout-sider">
-          <div className="layout-logo">
-              <i className="iconfont icon-jianli" id="logo"></i>
-              {!collapse && <span className="title-text"> 张展亮 </span>}
-          </div>
-          <Menu mode="inline"
-              theme="dark"
-              defaultSelectedKeys={['home']}
-              selectedKeys={[current]}
-              onClick={handleClick}
-          >
-            {
-              MenuData.map( data => {
-                const name = data.name,
-                  desc = data.desc;
+      <aside className='layout-sider'>
+        <div className='layout-logo'>
+          <i className='iconfont icon-jianli' id='logo' />
+          {!collapse && <span className='title-text'> 马俊昌 </span>}
+        </div>
+        <Menu mode='inline'
+          theme='dark'
+          defaultSelectedKeys={['home']}
+          selectedKeys={[current]}
+          onClick={handleClick}
+        >
+          {
+            MenuData.map(data => {
+              const name = data.name,
+                desc = data.desc
 
-                return (
-                  <Menu.Item key={name}>
-                      <Link to={name == "home" ? "/" : `/${name}`}>
-                          <i className={name == "home" ? "iconfont icon-index" : `iconfont icon-${name}`}></i>
-                          {!collapse && <span className="nav-text">{desc}</span>}
-                      </Link>
-                  </Menu.Item>
-                )
-              })
-            }
-          </Menu>
-          <div className="aside-action" onClick={onCollapseChange}>
-              {collapse ? <Icon type="right" /> : <Icon type="left" />}
-          </div>
+              return (
+                <Menu.Item key={name}>
+                  <Link to={name == 'home' ? '/' : `/${name}`}>
+                    <i className={name == 'home' ? 'iconfont icon-index' : `iconfont icon-${name}`} />
+                    {!collapse && <span className='nav-text'>{desc}</span>}
+                  </Link>
+                </Menu.Item>
+              )
+            })
+          }
+        </Menu>
+        <div className='aside-action' onClick={onCollapseChange}>
+          {collapse ? <Icon type='right' /> : <Icon type='left' />}
+        </div>
       </aside>
 
     )
